28f5ce7bc773bcf9e8220c48ec08c8c2463a038a
Elaine Muhombe <emuhombe@gmail.com>
2024-09-08 20:56:37 +0300

    feat: Implement service to fetch job applications by job ID
    - Added  function to retrieve job applications for a specific job ID.
    - Implemented error handling for cases where no applications are found or if an error occurs during fetching.
    - Added logging for debugging purposes in case of errors.

--- src/services/job-application.service.ts
+++ src/services/job-application.service.ts
@@ -1,9 +1,10 @@
 import { JobApplication } from '../models/job-application.model';
 
+// Service to apply for a job
 export const applyForJobService = async (
-	job_id: string, 
-	job_seeker_id: string, 
-	cover_letter: string, 
+	job_id: string,
+	job_seeker_id: string,
+	cover_letter: string,
 	resume: string
 ) => {
 	// Check if there is already an application from this job seeker for the specified job
@@ -18,4 +19,23 @@ export const applyForJobService = async (
 	// Save the new application to the database
 	const savedApplication = await newApplication.save();
 	return savedApplication;
+};
+
+// Service to get job applications By Job Id made by job seekers
+export const getJobApplicationsByJobIdService = async (job_id: string) => {
+	try {
+		// Find all job applications made by a specific job seeker
+		const jobApplicationsById = await JobApplication.find({ job_id });
+
+		// Check if no job applications were found
+		if (jobApplicationsById.length === 0) {
+			throw new Error(`No job applications found for job ID: ${job_id}`);
+		}
+
+		// Return the job applications
+		return jobApplicationsById;
+	} catch (error) {
+		console.error('Error fetching job applications:', error); // Log the error for debugging
+		throw new Error(`Error fetching job applications: ${error.message}`);
+	}
 };
