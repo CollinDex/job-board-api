f5a68f9545ff48af50c9b21dfa1720fdb69b217c
Elaine Muhombe <emuhombe@gmail.com>
2024-09-08 20:59:07 +0300

    feat: Define GET route for fetching job applications by job ID
    - Added route handler for GET /getAllJobs/:job_id
    - Connected route to  controller function

--- src/routes/job-application.route.ts
+++ src/routes/job-application.route.ts
@@ -1,15 +1,18 @@
 // Import the necessary modules
-import {Router} from 'express';
-import { applyForJob } from '../controllers/job-application.controller';
+import { Router } from 'express';
+import { applyForJob, getAllJobApplicationsById } from '../controllers/job-application.controller';
 
 // Create an instance of the express router
 const applicationRoute = Router();
 
 // Define a POST route to handle job application submissions
+// Endpoint: /apply
 applicationRoute.post('/apply', applyForJob);
+
+// Define a GET route to get all job applications by job ID
+// Endpoint: /getAllJobs/:job_id
+applicationRoute.get('/getAllJobs/:job_id', getAllJobApplicationsById);
 
 // Export the router to be used in other parts of the application
 
-export {applicationRoute}
-
-
+export { applicationRoute };
