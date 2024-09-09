9b2ffda09172a495f828a8f14bbb323d954f37e0
Elaine Muhombe <emuhombe@gmail.com>
2024-09-09 08:40:07 +0300

    refactor: Integrate validation middleware into job application routes

--- src/routes/job-application.route.ts
+++ src/routes/job-application.route.ts
@@ -1,13 +1,15 @@
 // Import the necessary modules
 import { Router } from 'express';
 import { applyForJob, getAllJobApplicationsById } from '../controllers/job-application.controller';
+import { validateData } from '../middleware/validationMiddleware';
+import { applyForJobSchema } from '../validation-schema/job-application.schema';
 
 // Create an instance of the express router
 const applicationRoute = Router();
 
 // Define a POST route to handle job application submissions
 // Endpoint: /apply
-applicationRoute.post('/apply', applyForJob);
+applicationRoute.post('/apply', validateData(applyForJobSchema, ['body']), applyForJob);
 
 // Define a GET route to get all job applications by job ID
 // Endpoint: /getAllJobs/:job_id
